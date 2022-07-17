824b5cba3d36afd34d32c8b1ef14f5b248295141
Mikhail Bootko <m14ai1@yandex.ru>
2022-07-17 18:51:52 +0300

    need master kick

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,12 @@
 import {disabledButton} from './modules/form';
 import {toggleMenu} from './modules/menu.js';
 
+const nosscriptMap = document.querySelector('noscript');
+const interactiveMap = document.querySelector('iframe.contacts__map');
+
+nosscriptMap.classList.add('contacts__map--noscript');
+interactiveMap.classList.remove('contacts__map--noscript');
+
+
 toggleMenu();
 disabledButton();
